5973d04986880bc0213b3a6142e560d88c921714
Amir Mobini <68190149+amirmbn@users.noreply.github.com>
2025-07-02 12:23:10 +0330

    Update backhaul_install.sh

--- backhaul_install.sh
+++ backhaul_install.sh
@@ -594,7 +594,7 @@ fi
 clear # Clear screen before displaying the final config and running Backhaul
 echo "------------------"
 echo "Configuration file ($CONFIG_FILE) created successfully:"
-echo "---------------------------------------------------------------------"
+echo "-------------------------------------------------------"
 echo ""
 
 # Create the systemd service file
